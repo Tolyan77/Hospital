1b6544652d58369f75064b6f29f85ffe9dd67319
Tolyan <tolyanchyk77@gmail.com>
2019-05-02 19:34:49 +0300

    Hospital first menu

---
+++ Hospital/Hospital/GolovneMenu.cpp
@@ -0,0 +1,9 @@
+#include "GolovneMenu.h"
+#include <iostream>
+using namespace std;
+int Choise()
+{
+	int choise = 0;
+	cout << "\n\t<<<<<<<<<<<<<<<<<<<< Hospital >>>>>>>>>>>>>>>>>>>\n\n\t1 - Review";
+	return choise;
+}
---
+++ Hospital/Hospital/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "GolovneMenu.h"
+#include <string>
+using namespace std;
+struct Person {
+public: 
+
+};
+
+
+
+int main()
+{
+	int choise = Choise();
+
+
+
+	system("pause");
+	return 0;
+}
